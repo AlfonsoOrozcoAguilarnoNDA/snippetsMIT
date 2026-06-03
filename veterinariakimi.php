c9c38c87b27ac82dccc93376b74e0b815e5a8ab1
AlfonsoOrozcoAguilarnoNDA <redrodac@gmail.com>
2026-06-03 12:51:12 -0600

    Update veterinariakimi.php

--- veterinariakimi.php
+++ veterinariakimi.php
@@ -21,7 +21,7 @@
  * furnished to do so, subject to the following conditions:
  * * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * https://vibecodingmexico.com/snippet-2-visor-imagenes/
+ * https://vibecodingmexico.com/control-veterinario-casero/
  */
 require_once 'config.php';
 
